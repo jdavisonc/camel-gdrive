fa6084c839e0ed073a5ef8bd3308f4e71987c694
jdavisonc <jdavisonc@gmail.com>
2013-04-04 21:37:45 -0300

    Initial import with component structure

---
+++ src/main/java/com/github/jdavisonc/Camel GDrive :: A Camel component for integrate Google DriveProducer.java
@@ -0,0 +1,40 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jdavisonc;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The Camel GDrive :: A Camel component for integrate Google Drive producer.
+ */
+public class Camel GDrive :: A Camel component for integrate Google DriveProducer extends DefaultProducer {
+    private static final transient Logger LOG = LoggerFactory.getLogger(Camel GDrive :: A Camel component for integrate Google DriveProducer.class);
+    private Camel GDrive :: A Camel component for integrate Google DriveEndpoint endpoint;
+
+    public Camel GDrive :: A Camel component for integrate Google DriveProducer(Camel GDrive :: A Camel component for integrate Google DriveEndpoint endpoint) {
+        super(endpoint);
+        this.endpoint = endpoint;
+    }
+
+    public void process(Exchange exchange) throws Exception {
+        System.out.println(exchange.getIn().getBody());    
+    }
+
+}
